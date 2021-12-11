c95c28b581a825747283795b026753fa7d8595bb
Zouheir Jamal <zouheirjamal6@gmail.com>
2021-12-11 03:29:06 +0200

    first 2 activities

--- app/src/main/java/com/example/projectthree/MainActivity.java
+++ app/src/main/java/com/example/projectthree/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.projectthree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondActivity();
+            }
+        });
+    }
+
+    private void openSecondActivity() {
+        Intent intent1 = new Intent(this, MainScreenActivity.class);
+        startActivity(intent1);
     }
 
 }
